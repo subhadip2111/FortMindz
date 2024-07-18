94d900966d84950f16f9877ade33d241d833e4f0
subhadip <subhadip@m0.ventures>
2024-07-18 23:12:26 +0530

    changes in filter option

--- src/controller/serviceController.js
+++ src/controller/serviceController.js
@@ -1,13 +1,13 @@
-const Service = require('../models/Service');
-const Saloon = require('../models/Saloon');
-const Beautician=require('../models/BeauticianModel')
+const Service = require("../models/Service");
+const Saloon = require("../models/Saloon");
+const Beautician = require("../models/BeauticianModel");
 exports.getAllServices = async (req, res) => {
   try {
-    const services = await Service.find().populate('saloon');
+    const services = await Service.find().populate("saloon");
     res.json(services);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server error');
+    res.status(500).send("Server error");
   }
 };
 
@@ -18,19 +18,19 @@ exports.getServicesByBeautician = async (req, res) => {
     // Find the beautician by ID
     const beautician = await Beautician.findById(beauticianId);
     if (!beautician) {
-      return res.status(404).json({ message: 'Beautician not found' });
+      return res.status(404).json({ message: "Beautician not found" });
     }
 
     const services = await Service.find({ beautician: beauticianId }).populate({
-      path: 'saloon',
-      model: 'Saloon',
-      select: 'name location address ratings' 
+      path: "saloon",
+      model: "Saloon",
+      select: "name location address ratings",
     });
 
     const result = {
       _id: beautician._id,
       name: beautician.name,
-      services: services.map(service => ({
+      services: services.map((service) => ({
         _id: service._id,
         name: service.name,
         saloon: {
@@ -38,28 +38,27 @@ exports.getServicesByBeautician = async (req, res) => {
           name: service.saloon.name,
           location: service.saloon.location,
           address: service.saloon.address,
-          ratings: service.saloon.ratings
-        }
-      }))
+          ratings: service.saloon.ratings,
+        },
+      })),
     };
 
     res.json(result);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 };
-//Query Filter 
+//Query Filter
 exports.searchServices = async (req, res) => {
-  const { keyword, priceMin,priceMax, serviceType, rating } = req.query;
+  const { keyword, priceMin, priceMax, serviceType, rating } = req.query;
   try {
     let matchQuery = {};
 
-    
     if (keyword) {
       matchQuery.$or = [
-        { 'name': { $regex: new RegExp(keyword, 'i') } },
-        { 'description': { $regex: new RegExp(keyword, 'i') } }
+        { name: { $regex: new RegExp(keyword, "i") } },
+        { description: { $regex: new RegExp(keyword, "i") } },
       ];
     }
 
@@ -74,12 +73,12 @@ exports.searchServices = async (req, res) => {
 
     // Service type filter
     if (serviceType) {
-      matchQuery.type = { $regex: new RegExp(serviceType, 'i') };
+      matchQuery.type = { $regex: new RegExp(serviceType, "i") };
     }
 
     // Rating filter
     if (rating) {
-      matchQuery['saloon.ratings.average'] = { $gte: parseFloat(rating) };
+      matchQuery["saloon.ratings.average"] = { $gte: parseFloat(rating) };
     }
 
     const pipeline = [];
@@ -90,27 +89,26 @@ exports.searchServices = async (req, res) => {
 
     pipeline.push({
       $lookup: {
-        from: 'saloons', 
-        localField: 'saloon',
-        foreignField: '_id',
-        as: 'saloon'
-      }
+        from: "saloons",
+        localField: "saloon",
+        foreignField: "_id",
+        as: "saloon",
+      },
     });
 
-    pipeline.push({ $unwind: '$saloon' });
+    pipeline.push({ $unwind: "$saloon" });
 
     pipeline.push({
       $lookup: {
-        from: 'beauticians', 
-        localField: 'saloon.beautician',
-        foreignField: '_id',
-        as: 'beautician'
-      }
+        from: "beauticians",
+        localField: "saloon.beautician",
+        foreignField: "_id",
+        as: "beautician",
+      },
     });
 
-    pipeline.push({ $unwind: '$beautician' });
+    pipeline.push({ $unwind: "$beautician" });
 
-  
     pipeline.push({
       $project: {
         _id: 1,
@@ -120,15 +118,15 @@ exports.searchServices = async (req, res) => {
         type: 1,
         saloon: {
           _id: 1,
-          name: '$saloon.name',
-          location: '$saloon.location',
-          ratings: '$saloon.ratings'
+          name: "$saloon.name",
+          location: "$saloon.location",
+          ratings: "$saloon.ratings",
         },
         beautician: {
-          _id: '$beautician._id',
-          name: '$beautician.name',
-        }
-      }
+          _id: "$beautician._id",
+          name: "$beautician.name",
+        },
+      },
     });
 
     const services = await Service.aggregate(pipeline);
@@ -136,21 +134,21 @@ exports.searchServices = async (req, res) => {
     res.json({ total: services.length, services });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server error');
+    res.status(500).send("Server error");
   }
 };
 exports.sortServices = async (req, res) => {
   const { sortBy } = req.query;
   try {
-    let services = await Service.find().populate('saloons');
-    if (sortBy === 'priceLowToHigh') {
+    let services = await Service.find().populate("saloons");
+    if (sortBy === "priceLowToHigh") {
       services.sort((a, b) => a.price - b.price);
-    } else if (sortBy === 'priceHighToLow') {
+    } else if (sortBy === "priceHighToLow") {
       services.sort((a, b) => b.price - a.price);
     }
     res.json(services);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server error');
+    res.status(500).send("Server error");
   }
 };
